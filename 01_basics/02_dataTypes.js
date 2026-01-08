23962b25af46f335591086a233ef9fc8425c130c
aniketpandey04 <aniketpandey1981@gmail.com>
2026-01-08 19:42:03 +0000

    dataType config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use Strict"; // means : teat all js code in newer version
+
+// alert( 3 + 3 );// we are using node js , not browser 
+
+
+console.log(3  
++ 3
+)// code readability should be high
+
+console.log("Hello World"); 
+
+let name="Aniket"
+let age=23
+let state;
+
+/*  
+  DataTypes: 
+  1)number = 2 to power 53
+  2) bitint= large no to number 
+  3) string = ""
+  4) boolean = true/false
+  5) null = standalone value
+  6) undefined = undefined
+  7) symbol= unique 
+  8) object = null 
+
+
+*/ 
+
+console.log(typeof age)  // number
+
+
+console.log(typeof null)// object 
+
+
+console.log(typeof undefined)//undefine
