feca7fc516059f8b317fc5a32f6c5be231a273eb
aniketpandey04 <aniketpandey1981@gmail.com>
2026-01-08 19:02:09 +0000

    variable config done

---
+++ 01_basics/01_varables.js
@@ -0,0 +1,13 @@
+const accountId=1234 
+let accoutEmail="ani@gmail.com" 
+var accountPassword=10002
+accountCity="Bangalore"
+
+let accountState;
+
+accountId=4321
+
+console.log(accountId)
+console.table([accountId, accoutEmail, accountPassword, accountCity, accountState]) 
+
+accoutEmail=
